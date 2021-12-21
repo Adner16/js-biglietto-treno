7422439c586f88ac03951b356237bc92e423908a
Adner16 <giovanni.renda16@gmail.com>
2021-12-21 17:58:11 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ console.log('JS OK')
 
 //0
 
-let finalPrice
+let finalPrice;
 
 //1
 const userKm = prompt('Per quanti km vuoi viaggiare?');
@@ -22,33 +22,32 @@ const userAge = prompt('Quanti anni hai?');
 console.log(`età del cliente:${userAge}`);
 
 //3
-const ticketPrice = userKm * 0.21
-console.log(`costo biglietto:${ticketPrice}`)
+const ticketPrice = userKm * 0.21;
+console.log(`costo biglietto:${ticketPrice}`);
 
 //4
 
 if (userAge < 18) {
-    const discountYoung = ticketPrice * 0.20
-    console.log(`sconto young: ${discountYoung}`)
-    finalPrice = (ticketPrice - discountYoung)
-    console.log(`prezzo scontato young: ${finalPrice}`)
+    const discountYoung = ticketPrice * 0.20;
+    console.log(`sconto young: ${discountYoung}`);
+    finalPrice = (ticketPrice - discountYoung).toFixed(2);
+    console.log(`prezzo scontato young: ${finalPrice}`);
 }
 else if (userAge >= 65) {
 
-    const discountSenior = ticketPrice * 0.40
-    console.log (`sconto senior: ${discountSenior}`)
-    finalPrice = (ticketPrice - discountSenior)
-    console.log(`prezzo scontato senior: ${finalPrice}`)
+    const discountSenior = ticketPrice * 0.40;
+    console.log (`sconto senior: ${discountSenior}`);
+    finalPrice = (ticketPrice - discountSenior);
+    console.log(`prezzo scontato senior: ${finalPrice}`);
 }
 else {
-    finalPrice = ticketPrice
-    console.log (`prezzo senza sconto: ${finalPrice}`)
+    finalPrice = ticketPrice;
+    console.log (`prezzo senza sconto: ${finalPrice}`);
 }
 
 // 5 
 
-const prezzo = document.getElementById('prezzo')
-parseFloat(num).toFixed(2);
+const prezzo = document.getElementById('prezzo');
 
 
-prezzo.innerHTML = finalPrice     
+prezzo.innerHTML = finalPrice.toFixed(2);   
